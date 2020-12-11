c794dcfbbd6ec3b736edec0b2a23875203af673f
Oscar Franzén <47160197+Noviceprogrammer000@users.noreply.github.com>
2020-12-11 12:10:09 +0100

    Add files via upload

---
+++ webb2 sida/js/betting.js
@@ -0,0 +1,40 @@
+  
+var k;
+ var credits = "1000";
+k = document.getElementById("coins");
+
+k = credits;
+ 
+
+
+/*(function() {
+  const wheel = document.querySelector('.resize');
+  const startButton = document.querySelector('.bild');
+  let deg = 0;
+
+  startButton.addEventListener('', () => {
+
+    startButton.style.pointerEvents = 'none';
+    
+    deg = Math.floor(5000 + Math.random() * 5000);
+    
+    wheel.style.transition = 'all 10s ease-out';
+   
+    wheel.style.transform = `rotate(${deg}deg)`;
+ 
+    wheel.classList.add('blur');
+  });
+
+  wheel.addEventListener('transitionend', () => {
+
+    wheel.classList.remove('blur');
+
+    startButton.style.pointerEvents = 'auto';
+  
+    wheel.style.transition = 'none';
+  
+    const actualDeg = deg % 360;
+
+    wheel.style.transform = `rotate(${actualDeg}deg)`;
+  });
+})();*/
